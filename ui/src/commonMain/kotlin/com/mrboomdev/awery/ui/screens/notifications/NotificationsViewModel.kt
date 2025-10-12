291e67e9589ce0a76a4dc77c3679c2a5de353b0e
codefactor-io <support@codefactor.io>
2025-10-12 19:27:35 +0000

    [CodeFactor] Apply fixes to commit 4d4df29
    [ci skip] [skip ci]

--- ui/src/commonMain/kotlin/com/mrboomdev/awery/ui/screens/notifications/NotificationsViewModel.kt
+++ ui/src/commonMain/kotlin/com/mrboomdev/awery/ui/screens/notifications/NotificationsViewModel.kt
@@ -2,5 +2,4 @@ package com.mrboomdev.awery.ui.screens.notifications
 
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel: ViewModel() {
-}
+class NotificationsViewModel: ViewModel()
